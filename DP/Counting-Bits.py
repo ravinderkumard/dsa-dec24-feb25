490b2acfead9ed837538591940bcc7fc082c2223
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-06-25 05:57:55 +0530

    [338] [Time Beats: 64.36%] [Memory Beats: 99.30%] - LeetPush

--- DP/Counting-Bits.py
+++ DP/Counting-Bits.py
@@ -1,14 +1,8 @@
 1class Solution:
 2    def countBits(self, n: int) -> List[int]:
 3        result = [0]*(n+1)
-4        for i in range(1,n+1):
-5            count = 0
-6            x = i
-7            while x:
-8                if result[x]!=0:
-9                    count+=result[x]
-10                    break
-11                x&=x-1
-12                count+=1
-13            result[i] = count
-14        return result
+4
+5        for i in range(1,n+1):
+6            result[i] = result[i>>1]+(i&1)
+7        
+8        return result
