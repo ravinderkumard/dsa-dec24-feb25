c1e5528afae25dccf344d685cd0d8dc8aeb958b8
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-06-29 06:47:24 +0530

    [140] [Time Beats: 100.00%] [Memory Beats: 53.78%] - LeetPush

--- DP/Word-Break-II.py
+++ DP/Word-Break-II.py
@@ -3,18 +3,21 @@
 3        map = {}
 4
 5        def dfs(s,wordDict):
-6            res = []
-7            n = len(s)
-8            for word in wordDict:
-9                if not s.startswith(word):
-10                    continue
-11                end = len(word)
-12                if end == n:
-13                    res.append(word)
-14                else:
-15                    subList = dfs(s[end:],wordDict)
-16                    for item in subList:
-17                        item = word+" "+item
-18                        res.append(item)
-19            return res
-20        return dfs(s,wordDict)
+6            if s in map:
+7                return map[s]
+8            res = []
+9            n = len(s)
+10            for word in wordDict:
+11                if not s.startswith(word):
+12                    continue
+13                end = len(word)
+14                if end == n:
+15                    res.append(word)
+16                else:
+17                    subList = dfs(s[end:],wordDict)
+18                    for item in subList:
+19                        item = word+" "+item
+20                        res.append(item)
+21            map[s] = res
+22            return res
+23        return dfs(s,wordDict)
