75f72f46b207ba3ef571e89b4ea605d44535c8f3
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-07 09:09:01 +0530

    [673] [Time Beats: 8.49%] [Memory Beats: 7.14%] - LeetPush

--- DP/673-Number-of-Longest-Increasing-Subsequence.py
+++ DP/673-Number-of-Longest-Increasing-Subsequence.py
@@ -2,31 +2,37 @@ class Solution:
     def findNumberOfLIS(self, nums: List[int]) -> int:
         n = len(nums)
 
-        memo = [[None]*(n+1) for _ in range(n)]
+        memo = [None] * n
 
-        def seq(curr,prev):
-            if curr == len(nums):
-                return (0,1)
+        def dp(i):
+            if memo[i] is not None:
+                return memo[i]
             
-            if memo[curr][prev+1] is not None:
-                return memo[curr][prev+1]
+            bestLen = 1
+            count = 1
 
-            takeLen = 0
-            takeCount = 0
-            if prev==-1 or nums[curr]>nums[prev]:
-                nextLen, nextCount = seq(curr+1,curr)
-                takeLen = 1+nextLen
-                takeCount = nextCount
+            for j in range(i+1,n):
+                if nums[j] > nums[i]:
+                    childLen, childCount = dp(j)
 
-            skipLen,skipCount = seq(curr+1,prev)
+                    if childLen+1>bestLen:
+                        bestLen = childLen+1
+                        count = childCount
+                    elif childLen+1==bestLen:
+                        count +=childCount
+            
+            memo[i] = (bestLen,count)
+            return memo[i]
+        
+        maxLen = 0
+        totalCount = 0
 
-            if takeLen > skipLen:
-                ans = (takeLen,takeCount)
-            elif skipLen>takeLen:
-                ans = (skipLen,skipCount)
-            else:
-                ans = (takeLen,takeCount+skipCount)
-            memo[curr][prev+1] = ans
-            return ans
-
-        return seq(0,-1)[1]
+        for i in range(n):
+            length,count = dp(i)
+            if length > maxLen:
+                maxLen = length
+                totalCount = count
+            elif length == maxLen:
+                totalCount+=count
+        
+        return totalCount
