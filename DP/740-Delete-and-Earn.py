ba234daf650cc3c3d6eca4999a634f4433e40fba
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-08 07:01:41 +0530

    [740] [Time Beats: 23.93%] [Memory Beats: 10.07%] - LeetPush

--- DP/740-Delete-and-Earn.py
+++ DP/740-Delete-and-Earn.py
@@ -3,16 +3,19 @@ class Solution:
     def deleteAndEarn(self, nums: List[int]) -> int:
         num_map = Counter(nums)
         max_number = max(nums)
+        memo = {}
         def count(num):
             if num <=0:
                 return 0
             if num == 1:
                 return num_map.get(num,0)
-            
+            if num in memo:
+                return memo[num]
             gain = num*num_map.get(num,0)
             not_take = count(num-1)
             take = gain + count(num-2)
-            return max(take,not_take)
+            memo[num] = max(take,not_take)
+            return memo[num]
 
 
         return count(max_number)
