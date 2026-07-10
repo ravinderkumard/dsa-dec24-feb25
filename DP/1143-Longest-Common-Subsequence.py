76fbc371f78d8f974841d85752dfc4c3bc6bef34
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-11 05:21:49 +0530

    [1143] [Time Beats: 22.22%] [Memory Beats: 37.79%] - LeetPush

--- DP/1143-Longest-Common-Subsequence.py
+++ DP/1143-Longest-Common-Subsequence.py
@@ -1,16 +1,21 @@
 class Solution:
     def longestCommonSubsequence(self, text1: str, text2: str) -> int:
-        
+        cache = [[None]*len(text2) for _ in range(len(text1))]
         def solve(idx1,idx2):
             if idx1==len(text1):
                 return 0
             if idx2==len(text2):
                 return 0
+            
+            if cache[idx1][idx2] is not None:
+                return cache[idx1][idx2]
+
             ans = 0
             if text1[idx1]==text2[idx2]:
                 ans = 1+solve(idx1+1,idx2+1)
             else:
                 ans = max(solve(idx1+1,idx2),solve(idx1,idx2+1))
+            cache[idx1][idx2] = ans
             return ans
 
         return solve(0,0)
