ac83bb5c0b07313d9a9227f02fd1a3e788b6985f
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-10 05:53:27 +0530

    [368] [Time Beats: 7.24%] [Memory Beats: 5.72%] - LeetPush

--- DP/368-Largest-Divisible-Subset.py
+++ DP/368-Largest-Divisible-Subset.py
@@ -2,36 +2,32 @@ class Solution:
     def largestDivisibleSubset(self, nums: List[int]) -> List[int]:
         nums.sort()
         n = len(nums)
-        memo = [[None]*(n+1) for _ in range(n+1)]
+        ans = []
+        cache = [[None]*(n+1) for _ in range(n+1)]
         def solve(curr,prev):
             if curr == n:
                 return 0
-            
-            if memo[curr][prev+1] is not None:
-                return memo[curr][prev+1]
-
-            not_take = solve(curr+1,prev)
-            take = -1
-            if prev == -1 or nums[curr]%nums[prev]==0:
+            if cache[curr][prev+1] is not None:
+                return cache[curr][prev+1]
+            take = 0
+            if prev==-1 or nums[curr]%nums[prev]==0:
                 take = 1+solve(curr+1,curr)
-            memo[curr][prev+1] = max(take,not_take)
-            return memo[curr][prev+1]
+            not_take = solve(curr+1,prev)
+            cache[curr][prev+1] = max(take,not_take)
+            return max(take,not_take)
         
         solve(0,-1)
-        ans = []
         curr = 0
         prev = -1
 
         while curr < n:
             take = -1
-            if prev == -1 or nums[curr]%nums[prev] == 0:
+            if prev == -1 or nums[curr]%nums[prev]==0:
                 take = 1+solve(curr+1,curr)
-            
-            skip = solve(curr+1,prev)
+            not_take = solve(curr+1,prev)
 
-            if take >=skip:
+            if take>=not_take:
                 ans.append(nums[curr])
                 prev = curr
             curr+=1
-        
         return ans
