b571795340006cc69b12e2548622a08a0386ca57
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-13 05:20:08 +0530

    [72] [Time Beats: N/A] [Memory Beats: N/A] - LeetPush

--- DP/72-Edit-Distance.py
+++ DP/72-Edit-Distance.py
@@ -1,24 +1,21 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        word1_len = len(word1)
-        word2_len = len(word2)
-        dp = [[0]*(word2_len+1) for _ in range(word1_len+1)]
-
-        for j in range(0,word2_len):
-            dp[word1_len][j] = word2_len-j
-
-        for i in range(0,word1_len):
-            dp[i][word2_len] = word1_len-i
-
-        for i in range(word1_len-1,-1,-1):
-            for j in range(word2_len-1,-1,-1):
-                if word1[i]==word2[j]:
-                    dp[i][j] = dp[i+1][j+1]
-                else:
-                    
-                    insert = dp[i][j+1]
-                    delete = dp[i+1][j]
-                    update = dp[i+1][j+1]
-                    dp[i][j] = 1+min(insert,update,delete)
         
-        return dp[0][0]
+        def editDistance(i,j):
+            if i == len(word1):
+                return len(word2)-j
+            
+            if j==len(word2):
+                return len(word1)-i
+
+            if word1[i]==word2[j]:
+                ans = editDistance(i+1,j+1)
+            else:
+                insert = editDistance(i,j+1)
+                delete = editDistance(i+1,j)
+                update = editDistance(i+1,j+1)
+                ans = 1+min(insert,update,delete)
+            
+            return ans
+
+        return editDistance(0,0)
