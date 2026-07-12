2015a81842f662c2add3654e629c987af62c66b6
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-13 05:23:23 +0530

    [72] [Time Beats: 89.87%] [Memory Beats: 83.29%] - LeetPush

--- DP/72-Edit-Distance.py
+++ DP/72-Edit-Distance.py
@@ -1,12 +1,15 @@
 class Solution:
     def minDistance(self, word1: str, word2: str) -> int:
-        
+        dp = [[None]*(len(word2)+1) for _ in range(len(word1)+1)]
         def editDistance(i,j):
             if i == len(word1):
                 return len(word2)-j
             
             if j==len(word2):
                 return len(word1)-i
+
+            if dp[i][j] is not None:
+                return dp[i][j]
 
             if word1[i]==word2[j]:
                 ans = editDistance(i+1,j+1)
@@ -15,7 +18,7 @@ class Solution:
                 delete = editDistance(i+1,j)
                 update = editDistance(i+1,j+1)
                 ans = 1+min(insert,update,delete)
-            
+            dp[i][j] = ans
             return ans
 
         return editDistance(0,0)
