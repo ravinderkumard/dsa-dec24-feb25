fcae7383166aecc55c7577ece7ea22e2fed97aa0
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-12 05:42:52 +0530

    [516] [Time Beats: 92.93%] [Memory Beats: 20.01%] - LeetPush

--- DP/516-Longest-Palindromic-Subsequence.py
+++ DP/516-Longest-Palindromic-Subsequence.py
@@ -1,19 +1,20 @@
 class Solution:
     def longestPalindromeSubseq(self, s: str) -> int:
-        cache = {}
+        n = len(s)
+        dp = [[None]*n for _ in range(n)]
         def solve(idx1,idx2):
             if idx1>idx2:
                 return 0
             if idx2==idx1:
                 return 1
-            if (idx1,idx2) in cache:
-                return cache[(idx1,idx2)]
+            if dp[idx1][idx2] is not None:
+                return dp[idx1][idx2]
             answer = 0
             if s[idx1]==s[idx2]:
                 answer = 2+solve(idx1+1,idx2-1)
             else:
                 answer = max(solve(idx1+1,idx2),solve(idx1,idx2-1))
-            cache[(idx1,idx2)] = answer
+            dp[idx1][idx2] = answer
             return answer
 
 
