f56a9ce6c9fa0580342d6b5efe4ab22ffb22e4a4
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-14 05:04:07 +0530

    [712] [Time Beats: 22.97%] [Memory Beats: 19.94%] - LeetPush

--- DP/712-Minimum-ASCII-Delete-Sum-for-Two-Strings.py
+++ DP/712-Minimum-ASCII-Delete-Sum-for-Two-Strings.py
@@ -1,7 +1,7 @@
 class Solution:
     def minimumDeleteSum(self, s1: str, s2: str) -> int:
         m,n = len(s1),len(s2)
-
+        dp = [[None]*(n+1) for _ in range(m+1)]
         def solve(i,j):
             if i==m:
                 return sum(ord(c) for c in s2[j:])
@@ -9,8 +9,12 @@ class Solution:
             if j==n:
                 return sum(ord(c) for c in s1[i:])
 
+            if dp[i][j] is not None:
+                return dp[i][j]
+
             if s1[i]==s2[j]:
-                return solve(i+1,j+1)
+                dp[i][j] = solve(i+1,j+1)
             else:
-                return min(ord(s1[i]) + solve(i+1,j), ord(s2[j])+solve(i,j+1))
+                dp[i][j] = min(ord(s1[i]) + solve(i+1,j), ord(s2[j])+solve(i,j+1))
+            return dp[i][j]
         return solve(0,0)
