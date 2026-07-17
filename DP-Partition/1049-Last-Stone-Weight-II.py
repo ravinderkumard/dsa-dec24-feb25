9b5b011d4d4806cae4e266f538a44fd71ffe8568
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-18 04:55:32 +0530

    [1049] [Time Beats: 83.25%] [Memory Beats: 84.16%] - LeetPush

--- DP-Partition/1049-Last-Stone-Weight-II.py
+++ DP-Partition/1049-Last-Stone-Weight-II.py
@@ -3,22 +3,14 @@ class Solution:
         total = sum(stones)
         n = len(stones)
         target = total//2
-        dp = [[None] * (n) for _ in range(target+1)]
-        def dfs(i,curr):
-            if curr==0:
-                return True
-            if i == len(stones) or curr<0:
-                return False
-            
-            if dp[curr][i] is not None:
-                return dp[curr][i]
-            
-            take = dfs(i+1,curr-stones[i])
-            skip = dfs(i+1,curr)
-            dp[curr][i] = take or skip
-            return take or skip
+
+        dp = [False] * (target+1)
+        dp[0] = True
+
+        for stone in stones:
+            for s in range(target,stone-1,-1):
+                dp[s] = dp[s] or dp[s-stone]
 
         for s in range(target,-1,-1):
-            if dfs(0,s):
-                return total-2*s
-        return 0
+            if dp[s]:
+                return total -2*s 
