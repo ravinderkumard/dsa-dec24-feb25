b27c985780601465248ae98c23a216ae994649a6
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-19 05:30:00 +0530

    [1884] [Time Beats: 5.31%] [Memory Beats: 9.29%] - LeetPush

---
+++ DP-Partition/1884-Egg-Drop-With-2-Eggs-and-N-Floors.py
@@ -0,0 +1,26 @@
+class Solution:
+    def twoEggDrop(self, n: int) -> int:
+        memo = {}
+
+        def dfs(floors, eggs):
+            if floors <= 1:
+                return floors
+
+            if eggs == 1:
+                return floors
+
+            if (floors, eggs) in memo:
+                return memo[(floors, eggs)]
+
+            ans = float("inf")
+
+            for x in range(1, floors + 1):
+                broken = dfs(x - 1, eggs - 1)
+                survive = dfs(floors - x, eggs)
+
+                ans = min(ans, 1 + max(broken, survive))
+
+            memo[(floors, eggs)] = ans
+            return ans
+
+        return dfs(n, 2)
