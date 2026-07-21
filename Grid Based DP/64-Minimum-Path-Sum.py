f4e340c3b93b7b5ff205da58dc379958475b1027
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-21 05:44:24 +0530

    [64] [Time Beats: 92.69%] [Memory Beats: 93.66%] - LeetPush

--- Grid Based DP/64-Minimum-Path-Sum.py
+++ Grid Based DP/64-Minimum-Path-Sum.py
@@ -2,17 +2,15 @@ class Solution:
     def minPathSum(self, grid: List[List[int]]) -> int:
         rows = len(grid)
         cols = len(grid[0])
-        dp = [[0]*(cols) for _ in range(rows)]
-        dp[0][0] = grid[0][0]
+        dp = [0]*(cols)
+        dp[0] = grid[0][0]
 
         for i in range(1,cols):
-            dp[0][i] = grid[0][i]+dp[0][i-1]
-        print(dp)
-        for i in range(1,rows):
-            dp[i][0] = grid[i][0]+dp[i-1][0]
-
-        for i in range(1,rows):
-            for j in range(1,cols):
-                    dp[i][j] = grid[i][j]+min(dp[i][j-1],dp[i-1][j])
+            dp[i] = grid[0][i]+dp[i-1]
         
-        return dp[rows-1][cols-1]
+        for i in range(1,rows):
+            dp[0]+=grid[i][0]
+            for j in range(1,cols):
+                    dp[j] = grid[i][j]+min(dp[j-1],dp[j])
+        
+        return dp[cols-1]
