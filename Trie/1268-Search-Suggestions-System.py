044549206cdbb2036613782feb2f14de18fb0d60
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-28 05:50:14 +0530

    [1268] [Time Beats: 83.01%] [Memory Beats: 94.16%] - LeetPush

--- Trie/1268-Search-Suggestions-System.py
+++ Trie/1268-Search-Suggestions-System.py
@@ -1,33 +1,19 @@
-class TrieNode:
-    def __init__(self):
-        self.children = {}
-        self.suggestions = []
-
 class Solution:
     def suggestedProducts(self, products: List[str], searchWord: str) -> List[List[str]]:
         products.sort()
-        root = TrieNode()
-
-        for product in products:
-            node = root
-            for ch in product:
-                if ch not in node.children:
-                    node.children[ch] = TrieNode()
-                
-                node = node.children[ch]
-
-                if len(node.suggestions)<3:
-                    node.suggestions.append(product)
-        
-        ans = []
-        node = root
+        answer = []
+        prefix = ""
 
         for ch in searchWord:
-            if node and ch in node.children:
-                node = node.children[ch]
-                ans.append(node.suggestions)
-            else:
-                node = None
-                ans.append([])
+            prefix+=ch
+            idx = bisect_left(products,prefix)
+
+            suggestions = []
+
+            for i in range(idx,min(idx+3,len(products))):
+                if products[i].startswith(prefix):
+                    suggestions.append(products[i])
+            
+            answer.append(suggestions)
         
-        return ans
+        return answer
