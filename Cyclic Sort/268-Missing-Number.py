e6ea14330f8750dea83baffa8ba1618d07905b9f
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-07-31 05:13:44 +0530

    [268] [Time Beats: 45.04%] [Memory Beats: 74.16%] - LeetPush

--- Cyclic Sort/268-Missing-Number.py
+++ Cyclic Sort/268-Missing-Number.py
@@ -1,6 +1,7 @@
 class Solution:
     def missingNumber(self, nums: List[int]) -> int:
         num_len = len(nums)
-        sum_actual = (num_len*(num_len+1))//2
-        sum_given = sum(nums)
-        return sum_actual-sum_given
+        for i in range(0,num_len):
+            num_len = num_len^i^nums[i]
+        
+        return num_len
