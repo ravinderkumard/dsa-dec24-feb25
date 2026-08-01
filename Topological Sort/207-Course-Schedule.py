2e5981d24bd3fb79de60e621c142e9db8f84abb7
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-08-02 05:29:11 +0530

    [207] [Time Beats: 86.27%] [Memory Beats: 18.64%] - LeetPush

---
+++ Topological Sort/207-Course-Schedule.py
@@ -0,0 +1,39 @@
+from collections import defaultdict
+from typing import List
+class Solution:
+    def canFinish(self, numCourses: int, prerequisites: List[List[int]]) -> bool:
+
+        graph = defaultdict(list)
+        for course,pre_course in prerequisites:
+            graph[course].append(pre_course)
+
+        print(graph)
+
+        state = [0] * numCourses
+
+        def dfs(course):
+            if state[course] == 1:
+                return False
+            
+            if state[course] == 2:
+                return True
+            
+            state[course] = 1
+
+            for pre_c in graph[course]:
+                if dfs(pre_c) == False:
+                    return False
+            
+            state[course] = 2
+
+            return True
+        
+        for course in range(numCourses):
+            if dfs(course)==False:
+                return False
+        
+        return True
+
+        return False
+
+
