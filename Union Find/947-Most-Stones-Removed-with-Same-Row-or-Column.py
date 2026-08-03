3d95dccbb4f4cb117e9abbd998a1fb181dff7370
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-08-04 05:18:01 +0530

    [947] [Time Beats: 73.21%] [Memory Beats: 86.79%] - LeetPush

--- Union Find/947-Most-Stones-Removed-with-Same-Row-or-Column.py
+++ Union Find/947-Most-Stones-Removed-with-Same-Row-or-Column.py
@@ -1,44 +1,35 @@
 
 class UnionFind:
-    def __init__(self,n):
-        self.parent = list(range(n))
-        self.rank = [0]*n
-
+    def __init__(self):
+        self.parent = {}
+        
     def find(self,x):
+        if x not in self.parent:
+            self.parent[x] = x
+        
         if self.parent[x]!=x:
             self.parent[x] = self.find(self.parent[x])
+        
         return self.parent[x]
-    
+
     def union(self,x,y):
         px = self.find(x)
         py = self.find(y)
 
-        if px==py:
-            return
-        
-        if self.rank[px]<self.rank[py]:
+        if px!=py:
             self.parent[px] = py
-        elif self.rank[px]>self.rank[py]:
-            self.parent[py] = px
-        else:
-            self.parent[py] = px
-            self.rank[px]+=1
-        
 
 class Solution:
     def removeStones(self, stones: List[List[int]]) -> int:
-        rows = len(stones)
-        cols = len(stones[0])
-        dsu = UnionFind(rows)
+        
+        dsu = UnionFind()
 
-        for i in range(rows):
-            for j in range(i+1,rows):
-                if stones[i][0]==stones[j][0] or stones[i][1]==stones[j][1]:
-                    dsu.union(i,j)
-        
+        for r,c in stones:
+            dsu.union(r,~c)
+
         components = set()
-        for i in range(rows):
-            components.add(dsu.find(i))
-        
-        return rows-len(components)
-                
+
+        for r,_ in stones:
+            components.add(dsu.find(r))
+
+        return len(stones) - len(components)        
