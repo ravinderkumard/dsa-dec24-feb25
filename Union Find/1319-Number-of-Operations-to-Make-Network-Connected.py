d496186cd43dc652807dd6fe003a346959958c94
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-08-08 05:38:02 +0530

    [1319] [Time Beats: 69.63%] [Memory Beats: 16.11%] - LeetPush

--- Union Find/1319-Number-of-Operations-to-Make-Network-Connected.py
+++ Union Find/1319-Number-of-Operations-to-Make-Network-Connected.py
@@ -1,41 +1,28 @@
-class UnionFind:
-    def __init__(self,n):
-        self.parent = {i: i for i in range(n)}
-        self.rank = [0]*n
-    
-    def find(self,x):
-        result = x
-        while self.parent[result]!=result:
-            self.parent[result] = self.parent[self.parent[result]]
-            result = self.parent[result]
-        return result
-    
-    def union(self,x,y):
-        px = self.find(x)
-        py = self.find(y)
-
-        if px==py:
-            return False
-        
-        if self.rank[px]>self.rank[py]:
-            self.parent[py] = px
-            self.rank[px]+=self.rank[py]
-        else:
-            self.parent[px] = py
-            self.rank[py]+=self.rank[px]
-        return True
-
 class Solution:
     def makeConnected(self, n: int, connections: List[List[int]]) -> int:
+        
+
+        adj_list = [[] for _ in range(n)]
+
         if len(connections)<n-1:
             return -1
-        uf = UnionFind(n)
-        count = 0
+
         for u,v in connections:
-            uf.union(u,v)
+            adj_list[u].append(v)
+            adj_list[v].append(u)
+
+        visited = [False] * n
+        components = 0
+
+        def dfs(node):
+            visited[node] = True
+            for next in adj_list[node]:
+                if not visited[next]:
+                    dfs(next)
 
         for i in range(n):
-            if uf.parent[i]==i:
-                count+=1
-        return count-1
-        
+            if not visited[i]:
+                components+=1
+                dfs(i)
+
+        return components-1
