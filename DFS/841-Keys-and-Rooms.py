7d0d43b79981ffa6a8b85e1ae3e3f45d3b592263
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-08-13 05:38:14 +0530

    [841] [Time Beats: 100.00%] [Memory Beats: 48.22%] - LeetPush

--- DFS/841-Keys-and-Rooms.py
+++ DFS/841-Keys-and-Rooms.py
@@ -1,20 +1,12 @@
 class Solution:
     def canVisitAllRooms(self, rooms: List[List[int]]) -> bool:
-        no_of_rooms = len(rooms)
-
-        self.count = 1
-        visited_key = [0]
-        def dfs(idx):
-              
-            key_list = rooms[idx]
-
-            for each_key in key_list:
-                if each_key in visited_key:
-                    continue
-                visited_key.append(each_key)  
-                self.count+=1
-                dfs(each_key)
+        visited = set()
+        stack = [0]
+        while stack:
+            key = stack.pop()
+            visited.add(key)
+            for next in rooms[key]:
+                if next not in visited:
+                    stack.append(next)
         
-        dfs(0)
-        print(self.count)
-        return self.count==no_of_rooms
+        return len(rooms)==len(visited)
