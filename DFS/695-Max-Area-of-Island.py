38833f252f015519310dad8440f212c7161d96f1
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-08-15 05:45:22 +0530

    [695] [Time Beats: 84.62%] [Memory Beats: 53.01%] - LeetPush

---
+++ DFS/695-Max-Area-of-Island.py
@@ -0,0 +1,18 @@
+class Solution:
+    def maxAreaOfIsland(self, grid: List[List[int]]) -> int:
+        rows = len(grid)
+        cols = len(grid[0])
+
+        def dfs(i,j):
+            
+            if i<0 or i>=rows or j<0 or j>=cols or grid[i][j] == 0:
+                return 0
+            grid[i][j]=0
+            return 1+dfs(i+1,j)+dfs(i,j+1)+dfs(i-1,j)+dfs(i,j-1)
+        maxArea = 0
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j]==1:
+                    maxArea = max(maxArea,dfs(i,j))
+        
+        return maxArea
