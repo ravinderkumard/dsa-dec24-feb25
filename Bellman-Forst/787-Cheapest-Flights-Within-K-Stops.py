0623e6e5d7cad337c9007bf7a52970265db2886f
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-08-23 06:04:44 +0530

    [787] [Time Beats: 88.77%] [Memory Beats: 72.45%] - LeetPush

--- Bellman-Forst/787-Cheapest-Flights-Within-K-Stops.py
+++ Bellman-Forst/787-Cheapest-Flights-Within-K-Stops.py
@@ -1,19 +1,30 @@
+from collections import defaultdict
 class Solution:
     def findCheapestPrice(self, n: int, flights: List[List[int]], src: int, dst: int, k: int) -> int:
+        graph = defaultdict(list)
+
+        for frm,to,price in flights:
+            graph[frm].append((to,price))
+
         INF = float("inf")
         cost = [INF]*n
         cost[src] = 0
 
+        queue = [src]
+
         for _ in range(k+1):
             next_cost = cost.copy()
+            next_queue = []
 
-            for frm,to,price in flights:
-                if cost[frm]==INF:
-                    continue
-                
-                next_cost[to] = min(next_cost[to],cost[frm]+price)
+            for city in queue:
+                for neighbor,price in graph[city]:
+                    new_cost = cost[city]+price
 
-            cost = next_cost
+                    if new_cost<next_cost[neighbor]:
+                        next_cost[neighbor] = new_cost
+                        next_queue.append(neighbor)
+            cost= next_cost
+            queue = next_queue
         
-        return -1 if cost[dst] == INF else cost[dst]
-    
+        return -1 if cost[dst]==INF else cost[dst]
+        
