686beb33d3a541009119f0589bfad4f4a6db4fea
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-08-25 06:29:52 +0530

    [518] [Time Beats: 45.03%] [Memory Beats: 44.77%] - LeetPush

--- Backtracking_Recursion/518-Coin-Change-II.py
+++ Backtracking_Recursion/518-Coin-Change-II.py
@@ -1,22 +1,19 @@
 class Solution:
     def change(self, amount: int, coins: List[int]) -> int:
-        memo = {}
+        n = len(coins)
+        dp = [[0]*(amount+1) for _ in range(n+1)]
 
-        def backtrack(index,amount):
-            if amount==0:
-                return 1
-
-            if index==len(coins) or amount<0:
-                return 0
-
-            if (index,amount) in memo:
-                return memo[(index,amount)]
-
-            take = backtrack(index,amount-coins[index])
-            skip = backtrack(index+1,amount)
-            memo[(index,amount)] = take+skip
-            return take+skip
+        for i in range(n+1):
+            dp[i][0]=1
         
-        return backtrack(0,amount)
+        for i in range(n-1,-1,-1):
+            for curr_amount in range(1,amount+1):
 
+                skip = dp[i+1][curr_amount]
+                take = 0
+                if coins[i]<=curr_amount:
+                    take = dp[i][curr_amount - coins[i]]
+                
+                dp[i][curr_amount] = take+skip
         
+        return dp[0][amount]
