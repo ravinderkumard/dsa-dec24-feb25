140744b6e03c77b7d95cae0558f520e928a5f9ee
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-08-27 05:47:35 +0530

    [279] [Time Beats: 7.08%] [Memory Beats: 7.64%] - LeetPush

--- DP/279-Perfect-Squares.py
+++ DP/279-Perfect-Squares.py
@@ -1,10 +1,12 @@
 class Solution:
-    # Time limit Exceed
     def numSquares(self, n: int) -> int:
+        memo = {}
         def solve(x):
             if x==0:
                 return 0
-            
+            if x in memo:
+                return memo[x]
+
             ans = float("inf")
             j = 1
             while j*j<=x:
@@ -12,6 +14,7 @@ class Solution:
                 ans = min(ans,1+solve(x-square))
                 j+=1
             
+            memo[x] = ans
             return ans
         
         return solve(n)
