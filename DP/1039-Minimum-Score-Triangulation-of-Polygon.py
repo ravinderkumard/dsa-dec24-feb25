2d9008af109cfbe2cae13881d5391cb3fd0dc661
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-08-31 06:21:57 +0530

    [1039] [Time Beats: 22.06%] [Memory Beats: 46.13%] - LeetPush

--- DP/1039-Minimum-Score-Triangulation-of-Polygon.py
+++ DP/1039-Minimum-Score-Triangulation-of-Polygon.py
@@ -1,17 +1,18 @@
 class Solution:
     def minScoreTriangulation(self, values: List[int]) -> int:
         n = len(values)
-        memo = {}
+        memo = [[-1]*n for _ in range(n)]
+
         def dfs(i,j):
             if j-i<2:
                 return 0
-            if (i,j) in memo:
-                return memo[(i,j)]
+            if memo[i][j] != -1:
+                return memo[i][j]
             answer = float("inf")
             for k in range(i+1,j):
                 cost = dfs(i,k)+dfs(k,j)+values[i]*values[k]*values[j]
                 answer = min(cost,answer)
-            memo[(i,j)] = answer
+            memo[i][j] = answer
             return answer
         
         return dfs(0,n-1)
