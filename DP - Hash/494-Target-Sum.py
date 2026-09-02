7dc3a30f307688019870bc9ffda10929ea279acb
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-09-03 05:15:12 +0530

    [494] [Time Beats: 64.08%] [Memory Beats: 40.32%] - LeetPush

--- DP - Hash/494-Target-Sum.py
+++ DP - Hash/494-Target-Sum.py
@@ -2,7 +2,10 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         n = len(nums)
         sum_val = sum(nums)
+        if abs(target) > sum_val:
+            return 0
         memo = [[-1]*(2*sum_val+1) for _ in range(n+1)]
+
         def solve(idx,total):
             if idx==n:
                 if total == target:
