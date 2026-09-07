604495fb1d957251786bd75931de7fd3ebe8d2e8
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-09-07 05:35:53 +0530

    [174] [Time Beats: 21.14%] [Memory Beats: 13.99%] - LeetPush

--- Matrix 2D/174-Dungeon-Game.py
+++ Matrix 2D/174-Dungeon-Game.py
@@ -2,23 +2,26 @@ class Solution:
     def calculateMinimumHP(self, dungeon: List[List[int]]) -> int:
         rows = len(dungeon)
         cols = len(dungeon[0])
-        memo = {}
+        memo = [[None]*cols for _ in range(rows)]
         def getVal(i,j):
-            if i == rows or j==cols:
+
+            if i==rows or j==cols:
                 return float("inf")
             
             if i==rows-1 and j==cols-1:
                 return max(1,1-dungeon[i][j])
             
-            if (i,j) in memo:
-                return memo[(i,j)]
+            if memo[i][j] is not None:
+                return memo[i][j]
 
-            if_we_go_right = getVal(i,j+1)
-            if_we_go_down = getVal(i+1,j)
+            right_move = getVal(i,j+1)
+            down_move = getVal(i+1,j)
 
-            min_health_required = min(if_we_go_right,if_we_go_down) - dungeon[i][j]
+            next_health = min(right_move,down_move)
 
-            memo[(i,j)] = max(1,min_health_required)
-            return memo[(i,j)]
+            required_health = next_health-dungeon[i][j]
 
+            memo[i][j] = max(1,required_health)
+            return max(1,required_health)
+        
         return getVal(0,0)
