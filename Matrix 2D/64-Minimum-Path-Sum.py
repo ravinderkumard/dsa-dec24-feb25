00fe757f2ca44a3c165c3bb3a8f62696704920b6
Ravinder Kumar <64346129+ravinderkumard@users.noreply.github.com>
2026-09-08 05:32:20 +0530

    [64] [Time Beats: 91.71%] [Memory Beats: 47.97%] - LeetPush

--- Matrix 2D/64-Minimum-Path-Sum.py
+++ Matrix 2D/64-Minimum-Path-Sum.py
@@ -3,19 +3,30 @@ class Solution:
         rows = len(grid)
         cols = len(grid[0])
         INF = float("inf")
-        memo = [[INF]*cols for _ in range(rows)]
+        memo = [[INF]*(cols+1) for _ in range(rows+1)]
 
-        def minPath(i,j):
-            if i==rows or j==cols:
-                return float("inf")
-            
-            if i==rows-1 and j==cols-1:
-                return grid[i][j]
+        memo[rows][cols-1] = 0
+        memo[rows-1][cols] = 0
 
-            if memo[i][j] != INF:
-                return memo[i][j]
-
-            memo[i][j] = grid[i][j] + min(minPath(i+1,j),minPath(i,j+1))
-            return memo[i][j]
+        for i in range(rows-1,-1,-1):
+            for j in range(cols-1,-1,-1):
+                memo[i][j] = grid[i][j] + min(memo[i][j+1],memo[i+1][j])
         
-        return minPath(0,0)
+        return memo[0][0]
+
+
+
+        # def minPath(i,j):
+        #     if i==rows or j==cols:
+        #         return float("inf")
+            
+        #     if i==rows-1 and j==cols-1:
+        #         return grid[i][j]
+
+        #     if memo[i][j] != INF:
+        #         return memo[i][j]
+
+        #     memo[i][j] = grid[i][j] + min(minPath(i+1,j),minPath(i,j+1))
+        #     return memo[i][j]
+        
+        # return minPath(0,0)
